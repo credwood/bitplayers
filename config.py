483bc957f4010321ad285432ba2a3602b1f0b146
Charysse <charysse.redwood@gmail.com>
2020-02-27 13:06:42 -0800

    baseconfig updated

--- config.py
+++ config.py
@@ -1,9 +1,18 @@
 import os
+import json
+
 basedir = os.path.abspath(os.path.dirname(__file__))
 
+with open('/etc/config.json') as config_file:
+        config = json.load(config_file)
 
 class BaseConfig:
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    SQLALCHEMY_DATABASE_URI = config.get('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = os.environ['SECRET_KEY']
+    SECRET_KEY = config.get('SECRET_KEY')
     POSTS_PER_PAGE = 6
+    MAIL_SERVER = 'smtp.gmail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = config.get('MAIL_USERNAME')
+    MAIL_PASSWORD = config.get('MAIL_PASSWORD')
